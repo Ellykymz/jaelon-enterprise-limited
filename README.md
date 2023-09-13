14fbbf6f04ac353c03dbc6c781086b91277a1f5b
Elly Sitoya <ellykymz254@gmail.com>
2023-09-13 06:49:42 +0300

    Improved the site

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+ <h1>Welcome to Jaelon Enterprise Limited Loan Management System!</h1>
+
+          <span>Grow With Joelon Enterprise</span>
+          <h2>Hustle Free Loans</h2>
+
+          <div className="start-button flexCenter">
+            <span>Get Started Today</span>
+          </div>
---
+++ src/utils/imageSlider.js
@@ -0,0 +1,23 @@
+export const imageSlider = [
+  {
+    title: "Get money in your Carbon wallet instantly",
+    info: "Once approved, we will disburse the funds in your account almost imediately.",
+    img: "https://ichef.bbci.co.uk/news/976/cpsprodpb/163A5/production/_123354019_mediaitem123346643.jpg",
+  },
+  {
+    title: "Apply for your desired loan",
+    info: "Go ahead and let us know how much you need. We will review your application and let you know where you stand.",
+    img: "https://media.istockphoto.com/id/502221279/photo/ripe-fruits-stacked-at-a-local-market-in-nairobi.jpg?s=612x612&w=0&k=20&c=4OxO4DmCdYluY_VBTlaIfWp_gZeMjzaRrbPdD8_gEm0=",
+  },
+
+  {
+    title: "No hidden fees",
+    info: "We’re invested in making loans affordable for you. Pay only what you owe. No late fees, rollover fees, or processing costs.",
+    img: "https://media.istockphoto.com/id/1087875468/photo/african-american-businessman-with-necktie-working-with-notebook.jpg?s=612x612&w=0&k=20&c=i0-L9Hz5zmllo95MH_0kCn6B7Ra0IXJp_r6i3eLx-jc=",
+  },
+  {
+    title: "Unlock higher amounts",
+    info: "Our loan process is designed to fit your needs and help you grow. Repay on time and unlock higher loan amounts and lower interest rates",
+    img: "https://c8.alamy.com/zooms/9/a6748d255b034fb9b307664d59a4bc72/c9bt42.jpg",
+  },
+];
