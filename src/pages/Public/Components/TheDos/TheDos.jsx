88b77981424f448ff8403bfa00a3cc0b82ef98ea
Elly Sitoya <ellykymz254@gmail.com>
2023-09-28 15:22:37 +0300

    updated courasel

--- src/pages/Public/Components/TheDos/TheDos.jsx
+++ src/pages/Public/Components/TheDos/TheDos.jsx
@@ -1,11 +1,23 @@
 import "./TheDos.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faCity,
+  faPooStorm,
+  faBinoculars,
+} from "@fortawesome/free-solid-svg-icons";
+
 const TheDos = () => {
+  const city = <FontAwesomeIcon icon={faCity} />;
+  const poorStorm = <FontAwesomeIcon icon={faPooStorm} />;
+  const binoculars = <FontAwesomeIcon icon={faBinoculars} />;
+
   return (
     <section className="container">
       <div className="container-wrapper theDos">
         <div className="theDos-container flexCenter">
           <div className="theDos-flex">
-            <h2>Logo</h2>
+            <h2>{city}</h2>
             <h2>Personal Loans</h2>
             <p>
               We’ve made applying for a personal loan simple, no matter what you
@@ -13,7 +25,7 @@ const TheDos = () => {
             </p>
           </div>
           <div className="theDos-flex">
-            <h2>Logo</h2>
+            <h2>{poorStorm}</h2>
             <h2>How to Apply</h2>
             <p>
               Everything you need to know about how to apply for a loan, and
@@ -21,7 +33,7 @@ const TheDos = () => {
             </p>
           </div>
           <div className="theDos-flex">
-            <h2>Logo</h2>
+            <h2>{binoculars}</h2>
             <h2>Locate Us</h2>
             <p>
               Find an Izwe branch nearest to you. We look forward to helping
